3302354b2b4d85c25d15be785ab3439ca8a67f81
DAChristina <d.christina23@imperial.ac.uk>
2025-03-30 01:05:17 +0700

    begin conditions

--- Advanced_R/1Intro_7_enviro.R
+++ Advanced_R/1Intro_7_enviro.R
@@ -39,7 +39,7 @@ library(rlang)
 
 # 2. Create an environment as illustrated by this picture
 # (enviro that contains itself).
-e1 <- env()
+e1 <- rlang::env()
 e1$loop <- e1
 
 env_print(e1)
@@ -47,8 +47,8 @@ lobstr::ref(e1)
 
 # 3. Create a pair of environments as illustrated by this picture
 # (two environments contain each other)
-e1 <- env()
-e2 <- env()
+e1 <- rlang::env()
+e2 <- rlang::env()
 
 e1$loop <- e2
 e2$dedoop <- e1
@@ -90,10 +90,107 @@ a
 #> [1] 10
 # How does it differ from <<- and why might you prefer it?
 # rebind() will only carry out an assignment when it finds an existing binding;
-# unlike <<- it will never create a new one in the global environment. 
+# unlike <<- it will never create a new one in the global environment.
+
 
 ################################################################################
 # 7.3.1 Exercises 
+# 1. Modify where() to return all environments that contain a binding for name.
+# Carefully think through what type of object the function will need to return.
+where <- function(name, env = caller_env()) {
+  if (identical(env, empty_env())) {
+    # Base case
+    stop("Can't find ", name, call. = FALSE)
+  } else if (env_has(env, name)) {
+    # Success case
+    env
+  } else {
+    # Recursive case
+    where(name, env_parent(env))
+  }
+}
+e1a <- rlang::env(empty_env(), a = 1, b = 2)
+e1b <- rlang::env(e1a, b = 10, c = 11)
+e1c <- rlang::env(e1b, a = 12, d = 13)
+where("a", e1c)
+
+where2 <- function(name, env = caller_env(), results = list()) {
+  if (identical(env, empty_env())) {
+    # Base case
+    results
+  } else {
+    # Recursive case
+    if (env_has(env, name)) {
+      results <- c(results, env)
+    }
+    where2(name, env_parent(env), results)
+  }
+}
+
+where2("a", e1c)
+
+# 2. Write a function called fget() that finds only function objects.
+# It should have two arguments, name and env,
+# and should obey the regular scoping rules for functions:
+# if there’s an object with a matching name that’s not a function,
+# look in the parent.
+# For an added challenge, also add an inherits argument which controls
+# whether the function recurses up the parents or only looks in one environment.
+fget <- function(name, env = caller_env(), inherits = TRUE) {
+  # Base case
+  if (env_has(env, name)) {
+    obj <- env_get(env, name)
+    
+    if (is.function(obj)) {
+      return(obj)
+    }
+  }
+  
+  if (identical(env, emptyenv()) || !inherits) {
+    stop("Could not find a function called \"", name, "\".",
+         call. = FALSE
+    )
+  }
+  
+  # Recursive Case
+  fget(name, env_parent(env))
+}
+
+# Test
+mean <- 10
+fget("mean", inherits = TRUE)
+
+
+################################################################################
+search()
+rlang::search_envs()
+# 7.4.5 Exercises 
+# 1. How is search_envs() different from env_parents(global_env())?
+# a chain of environments containing exported functions of attached packages.
+# env_parents(global_env()) will list all the ancestors of global,
+# therefore the global environment itself is not included.
+  
+# 2. Draw a diagram that shows the enclosing environments of this function:
+f1 <- function(x1) {
+  f2 <- function(x2) {
+    f3 <- function(x3) {
+      x1 + x2 + x3
+    }
+    f3(3)
+  }
+  f2(2)
+}
+f1(1)
+# f1 in parent (global) but f2 will only be created if f1 is executed etc.
+
+# 3. Write an enhanced version of str() that provides more information
+# about functions.
+# Show where the function was found and what environment it was defined in.
+# ?????
+
+
+
+
 
 
 
---
+++ Advanced_R/1Intro_8_conditions.R
@@ -0,0 +1,3 @@
+# Quiz
+# Answer the following questions to see if you can safely skip this chapter.
+# You can find the answers in Section 8.7.
